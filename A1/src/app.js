058d8f25658de81505c636132c488d4feb1c74fa
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2018-07-14 18:30:47 -0500

    webpack + scss

--- A1/src/app.js
+++ A1/src/app.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/main-app';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
