66b7799dd4cd5e73f1ff256a3b67d23a92e4ac39
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2018-07-29 17:02:12 -0500

    object spreading

--- xpenditure-app/src/playground/redux-xpenditure.js
+++ xpenditure-app/src/playground/redux-xpenditure.js
@@ -1,11 +1,90 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
+// ADD EXPENSE ACTION
+const addExpense = (
+    {
+        description = '',
+        note = '',
+        amount = 0,
+        createdAt = 0
+    } = {}
+) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
+
+
+// REMOVE EXPENSE ACTION
+const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
+
+// EDIT EXPENSE
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
+
+// SET TEXT FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
+// SORT BY DATE
+const sortByDate = () => ({
+    type: 'SORT_BY_DATE'
+});
+
+// SORT BY AMOUNT
+const sortByAmount = () => ({
+    type: 'SORT_BY_AMOUNT'
+});
+
+// SET START DATE
+const setStartDate = (startDate) => ({
+    type: 'SET_START_DATE',
+    startDate
+});
+
+// SET END DATE
+const setEndDate = (endDate) => ({
+    type: 'SET_END_DATE',
+    endDate
+});
 
 // EXPENSE REDUCER
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state,
+                action.expense
+            ];
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id }) => id !== action.id);
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    };
+                } else {
+                    return expense;
+                }
+            });
         default:
             return state;
     }
@@ -22,11 +101,35 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            };
+        case 'SORT_BY_DATE':
+            return {
+                ...state,
+                sortBy: 'date'
+            };
+        case 'SORT_BY_AMOUNT':
+            return {
+                ...state,
+                sortBy: 'amount'
+            };
+        case 'SET_START_DATE':
+            return {
+                ...state,
+                startDate: action.startDate
+            };
+        case 'SET_END_DATE':
+            return {
+                ...state,
+                endDate: action.endDate
+            };
         default:
-        return state;
+            return state;
     }
 };
-
 
 // STORE
 const store = createStore(
@@ -36,7 +139,27 @@ const store = createStore(
     })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState())
+});
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 14000 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Car Note', amount: 7000 }));
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 6000 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter(''));
+
+store.dispatch(sortByAmount(7000));
+store.dispatch(sortByDate());
+
+store.dispatch(setStartDate(350));
+store.dispatch(setStartDate());
+
+store.dispatch(setEndDate(450));
 
 const demoState = {
     expenses: [{
