5418f1c4947b670548133095e3b095e1bfe60943
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2018-08-15 15:03:26 -0500

    test fix

--- xpenditure-app/src/tests/setupTests.js
+++ xpenditure-app/src/tests/setupTests.js
@@ -4,3 +4,8 @@ import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({
     adapter: new Adapter()
 });
+
+// import { configure } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+
+// configure({ adapter: new Adapter() });
