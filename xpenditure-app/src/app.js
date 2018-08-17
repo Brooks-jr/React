f9026c9e525c28d01d7c532afc90d95cd494c951
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2018-08-17 14:47:54 -0500

    remove dummy data

--- xpenditure-app/src/app.js
+++ xpenditure-app/src/app.js
@@ -14,14 +14,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'water bill', amount: 300 }));
-store.dispatch(addExpense({ description: 'gas bill', createdAt: 939 }));
-store.dispatch(addExpense({ description: 'rent', amount: 1400 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store = {store}>
         <AppRouter />
